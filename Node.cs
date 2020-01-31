d53a347ded0457015f7017feade866afa6119c44
fatima <tima.alice806@gmail.com>
2020-01-31 08:11:46 -0600

    initial commit

---
+++ Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Node
+    {
+    }
+}
